6e723a9973ed71b270757bd6e373d0dc0c26498d
unknown <a@a.com>
2019-07-11 12:59:28 +0900

    Updates

---
+++ precache-manifest.368c47577311375536818a10a568ddf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23146a89fc3b64f6e8f32bd72dfe4ed8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b3d56b5ed0dc3b787891",
+    "url": "/monsters-rolodex/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/monsters-rolodex/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "b3d56b5ed0dc3b787891",
+    "url": "/monsters-rolodex/static/js/main.1b3fcb6b.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
